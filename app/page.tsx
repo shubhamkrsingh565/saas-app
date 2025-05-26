d60ab2aae958d3bc1ed9053562fa3cd7701ec319
Chotu Singh <chotu.sudarshandham@gmail.com>
2025-05-26 08:49:03 +0530

    implement companion card, list, cta, and form

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffdage"
+        />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Derivatives & Integrals"
+          subject="math"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language learning"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+          title="Recent completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
